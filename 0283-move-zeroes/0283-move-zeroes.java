8bf690710a04ffe129e44c0f3bd1258398e41dc5
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2026-08-11 19:40:51 +0530

    Time: 2 ms (92.06%), Space: 48.1 MB (6.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,24 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int write = 0;
+        // for( int read = 0; read < nums.length; read++){
+        //     if(nums[read]!= 0){
+        //         nums[write++] = nums[read];
+        //     }
+        // }
+        // for(; write < nums.length; write++){
+        //     nums[write] = 0;
+        // }
+
         int write = 0;
         for( int read = 0; read < nums.length; read++){
-            if(nums[read]!= 0){
-                nums[write++] = nums[read];
+            if(nums[read] != 0){
+                int temp = nums[write];
+                nums[write] = nums[read];
+                nums[read] = temp;
+                write++;
             }
         }
-        for(; write < nums.length; write++){
-            nums[write] = 0;
-        }
+        
     }
 }
